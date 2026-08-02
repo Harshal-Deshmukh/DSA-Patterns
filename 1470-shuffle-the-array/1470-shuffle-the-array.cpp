c83fd4540d0670e704a577eedeb382fb49d3a928
Harshal Deshmukh <25hdeshmukh@gmail.com>
2026-08-02 20:49:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count =0,max_len=0;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]==1){
+            count++;
+        }
+        else{
+            count =0;
+        }
+        max_len = max(count,max_len);
+       }
+       return max_len; 
+    }
+};
